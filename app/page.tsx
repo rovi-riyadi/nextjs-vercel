bd3002ce11403c303d529977021afd849b73af64
Rovi Riyadi <rovi.riyadi@harmony.co.id>
2024-01-15 10:54:58 +0700

    Ubah css padding

--- app/page.tsx
+++ app/page.tsx
@@ -2,11 +2,12 @@ import Image from 'next/image'
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex min-h-screen flex-col items-center justify-between pt-6 lg:p-24">
       <div className="z-10 max-w-5xl w-full items-center justify-center paytone-one-regular text-sm flex flex-col">
-        <span className={`mb-3 text-4xl font-semibold text-center`} style={{ letterSpacing: "0.5rem" }}>YUSUKA</span>
-        <span className={`mb-6 text-2xl text-center`}>Distributor Rokok Filter Lx, Plus, Djati, Dumanyar & Sinom.</span>
-        <span className={`mt-6 pt-6 text-3xl font-semibold text-center`}>Dapatkan diskon menarik untuk pelanggan yang order melalui whatsapp.</span>
+        <h1 className={`mb-3 text-4xl font-semibold text-center`} style={{ letterSpacing: "0.5rem" }}>YUSUKA</h1>
+        <h2 className={`mb-3 text-1xl font-semibold text-center`}>Yu Mau Semua Ada</h2>
+        <h2 className={`mb-6 text-2xl text-center`}>Distributor Rokok Filter Lx, Plus, Djati, Dumanyar & Sinom.</h2>
+        <h3 className={`mt-6 pt-6 text-3xl font-semibold text-center`}>Dapatkan diskon menarik untuk pelanggan yang order melalui whatsapp.</h3>
         {/* <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
           <h1 className={`mb-3 text-3xl font-semibold text-center`}>YUSUKA</h1>
           <code className="font-mono font-bold">app/page.tsx</code>
