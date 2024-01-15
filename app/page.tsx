08370b04e50017000917255bcf96520bc28694af
Rovi Riyadi <rovi.riyadi@harmony.co.id>
2024-01-15 10:21:08 +0700

    First Release

--- app/page.tsx
+++ app/page.tsx
@@ -3,19 +3,22 @@ import Image from 'next/image'
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
+      <div className="z-10 max-w-5xl w-full items-center justify-center paytone-one-regular text-sm flex flex-col">
+        <span className={`mb-3 text-4xl font-semibold text-center`} style={{ letterSpacing: "0.5rem" }}>YUSUKA</span>
+        <span className={`mb-6 text-2xl text-center`}>Distributor Rokok Filter Lx, Plus, Djati, Dumanyar & Sinom.</span>
+        <span className={`mt-6 pt-6 text-3xl font-semibold text-center`}>Dapatkan diskon menarik untuk pelanggan yang order melalui whatsapp.</span>
+        {/* <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
+          <h1 className={`mb-3 text-3xl font-semibold text-center`}>YUSUKA</h1>
           <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
+        </p> */}
+        {/* <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
           <a
             className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
             href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
             target="_blank"
             rel="noopener noreferrer"
           >
-            By{' '}
+            By{' Distributor Rokok Filter Lx, Plus, Djati, Dumanyar & Sinom.'}
             <Image
               src="/vercel.svg"
               alt="Vercel Logo"
@@ -25,10 +28,10 @@ export default function Home() {
               priority
             />
           </a>
-        </div>
+        </div> */}
       </div>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
+      {/* <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
         <Image
           className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
           src="/next.svg"
@@ -37,9 +40,10 @@ export default function Home() {
           height={37}
           priority
         />
-      </div>
+        <h1 className={`mb-3 text-2xl font-semibold`}>Dapatkan diskon menarik untuk pelanggan yang order melalui whatsapp.</h1>
+      </div> */}
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
+      {/* <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
         <a
           href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
           className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
@@ -107,9 +111,9 @@ export default function Home() {
             Instantly deploy your Next.js site to a shareable URL with Vercel.
           </p>
         </a>
-      </div>
+      </div> */}
       <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css" />
-      <a href="https://api.whatsapp.com/send?phone=+62859106782533&text=Halo+Arvi+Solusi+Nusantara%2C+saya+ingin+dibuatkan+aplikasi+untuk+keperluan+bisnis+saya." className="float" target="_blank">
+      <a href="https://api.whatsapp.com/send?phone=+6285207527000&text=Halo,+saya+tertarik+dengan+produk+Yusuka!+%0a+%0aRokok+Filter+%0aRokok Kretek+%0a+%0aMohon+info+lebih+lanjut.+Terima+kasih" className="float" target="_blank">
         <i className="fa fa-whatsapp my-float"></i>
       </a>
     </main>
